689ab5a4f0cbb06f17aeace0a8b79caa479b37fc
Fabio <torrejonfabio@gmail.com>
2026-01-30 19:04:03 -0300

    Base del proyecto con configuracion a la base de datos

---
+++ src/main/java/com/fabio/forohub/ForohubApiApplication.java
@@ -0,0 +1,13 @@
+package com.fabio.forohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForohubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForohubApiApplication.class, args);
+	}
+
+}
