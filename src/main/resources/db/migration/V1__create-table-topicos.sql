72db8fcb40e7b5abd6d62923b8820774a67e3f83
Fabio <torrejonfabio@gmail.com>
2026-01-31 18:56:04 -0300

    Migracion de la base de datos, creo la tabla topicos

---
+++ src/main/resources/db/migration/V1__create-table-topicos.sql
@@ -0,0 +1,12 @@
+create table topicos (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    activo tinyint NOT NULL,
+    titulo VARCHAR(255) NOT NULL,
+    mensaje TEXT NOT NULL,
+    fecha_creacion DATETIME NOT NULL,
+    estado VARCHAR(20) NOT NULL,
+    autor VARCHAR(255) NOT NULL,
+    curso VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (id)
+);
