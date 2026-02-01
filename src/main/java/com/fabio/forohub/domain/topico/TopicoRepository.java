b15a935808252a671c07e95efcc8e68a74f8cd1a
Fabio <torrejonfabio@gmail.com>
2026-01-31 21:35:25 -0300

    Agrego funciones necesarias

--- src/main/java/com/fabio/forohub/domain/topico/TopicoRepository.java
+++ src/main/java/com/fabio/forohub/domain/topico/TopicoRepository.java
@@ -1,8 +1,14 @@
 package com.fabio.forohub.domain.topico;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
-    boolean existsByTituloAndMensaje(String titulo, String mensaje);
+    boolean existsByTituloAndMensajeAndActivoTrue(String titulo, String mensaje);
+
+    boolean existsByTituloAndMensajeAndIdNotAndActivoTrue(String titulo, String mensaje, Long id);
+
+    Page<Topico> findAllByActivoTrue(Pageable paginacion);
 }
