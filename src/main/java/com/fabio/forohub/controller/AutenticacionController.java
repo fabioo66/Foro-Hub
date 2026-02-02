2cf6894b7956100888a7715009d7cae97e8cde3f
Fabio <torrejonfabio@gmail.com>
2026-02-02 14:16:20 -0300

    Controller para iniciar sesion

---
+++ src/main/java/com/fabio/forohub/controller/AutenticacionController.java
@@ -0,0 +1,29 @@
+package com.fabio.forohub.controller;
+
+import com.fabio.forohub.domain.usuario.DatosRegistroUsuario;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticacionController {
+
+    @Autowired
+    private AuthenticationManager manager;
+
+    @PostMapping
+    public ResponseEntity iniciarSesion(@RequestBody @Valid DatosRegistroUsuario datos) {
+        var token = new UsernamePasswordAuthenticationToken(datos.username(), datos.password());
+        var autenticacion = manager.authenticate(token);
+
+        return ResponseEntity.ok().build();
+    }
+
+}
