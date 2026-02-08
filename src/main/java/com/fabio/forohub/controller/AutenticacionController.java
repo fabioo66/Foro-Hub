3c7dddadeecdec6b845819958f08b60173f98b6f
Fabio <torrejonfabio@gmail.com>
2026-02-08 13:28:51 -0300

    Separo la logica de negocio del controller

--- src/main/java/com/fabio/forohub/controller/AutenticacionController.java
+++ src/main/java/com/fabio/forohub/controller/AutenticacionController.java
@@ -1,37 +1,41 @@
 package com.fabio.forohub.controller;
 
-import com.fabio.forohub.domain.usuario.DatosRegistroUsuario;
-import com.fabio.forohub.domain.usuario.Usuario;
+import com.fabio.forohub.domain.usuario.dto.DatosLoginUsuario;
+import com.fabio.forohub.domain.usuario.dto.DatosRegistroUsuario;
+import com.fabio.forohub.domain.usuario.dto.DatosRespuestaUsuario;
 import com.fabio.forohub.infra.security.DatosTokenJWT;
-import com.fabio.forohub.infra.security.TokenService;
+import com.fabio.forohub.service.AutenticacionService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
-@RequestMapping("/login")
+@RequestMapping("/auth")
 public class AutenticacionController {
 
     @Autowired
-    private TokenService tokenService;
+    private AutenticacionService autenticacionService;
 
-    @Autowired
-    private AuthenticationManager manager;
-
-    @PostMapping
-    public ResponseEntity iniciarSesion(@RequestBody @Valid DatosRegistroUsuario datos) {
-        var authenticationToken = new UsernamePasswordAuthenticationToken(datos.username(), datos.password());
-        var autenticacion = manager.authenticate(authenticationToken);
-
-        var tokenJWT = tokenService.generarToken((Usuario) autenticacion.getPrincipal());
-
+    @PostMapping("/login")
+    public ResponseEntity<DatosTokenJWT> iniciarSesion(@RequestBody @Valid DatosLoginUsuario datos) {
+        var tokenJWT = autenticacionService.autenticar(datos);
         return ResponseEntity.ok(new DatosTokenJWT(tokenJWT));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<DatosRespuestaUsuario> registrarUsuario(@RequestBody @Valid DatosRegistroUsuario datos,
+                                                                   UriComponentsBuilder uriComponentsBuilder) {
+        var usuario = autenticacionService.registrarUsuario(datos);
+
+        var uri = uriComponentsBuilder.path("/auth/usuarios/{id}")
+                .buildAndExpand(usuario.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DatosRespuestaUsuario(usuario));
     }
 
 }
--- src/main/java/com/fabio/forohub/service/AutenticacionService.java
+++ src/main/java/com/fabio/forohub/service/AutenticacionService.java
@@ -1,19 +1,61 @@
-package com.fabio.forohub.domain.usuario;
+package com.fabio.forohub.service;
 
+import com.fabio.forohub.infra.exception.ValidacionException;
+import com.fabio.forohub.domain.usuario.dto.DatosLoginUsuario;
+import com.fabio.forohub.domain.usuario.dto.DatosRegistroUsuario;
+import com.fabio.forohub.domain.usuario.Usuario;
+import com.fabio.forohub.domain.usuario.UsuarioRepository;
+import com.fabio.forohub.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class AutenticacionService implements UserDetailsService {
+public class AutenticacionService {
 
     @Autowired
     private UsuarioRepository repository;
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return repository.findByEmail(username);
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    public String autenticar(DatosLoginUsuario datos) {
+        var authenticationToken = new UsernamePasswordAuthenticationToken(datos.email(), datos.password());
+        var autenticacion = authenticationManager.authenticate(authenticationToken);
+
+        var usuario = (Usuario) autenticacion.getPrincipal();
+        return tokenService.generarToken(usuario);
+    }
+
+    @Transactional
+    public Usuario registrarUsuario(DatosRegistroUsuario datos) {
+        validarEmailUnico(datos.email());
+
+        var usuario = new Usuario(
+                null,
+                datos.nombre(),
+                datos.email(),
+                passwordEncoder.encode(datos.password()),
+                null
+        );
+
+        return repository.save(usuario);
+    }
+
+    private void validarEmailUnico(String email) {
+        var usuarioExistente = repository.findByEmail(email);
+
+        if (usuarioExistente != null) {
+            throw new ValidacionException("El email ya está registrado en el sistema.");
+        }
     }
 }
